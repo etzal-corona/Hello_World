2b4376eabab4c0157468ac05f8a4b9a1d90362db
Etzal Corona <Etzal Corona@DESKTOP-V1R3FDM>
2017-01-26 14:04:49 -0800

    Add project files.

---
+++ Hello_World/Hello_World.cpp
@@ -0,0 +1,16 @@
+// Hello_World.cpp : Defines the entry point for the console application.
+//
+
+#include "stdafx.h"
+#include <iostream>
+using namespace std;
+
+
+int main()
+{
+
+	cout << "Hello, World\n";
+	system("pause");
+    return 0;
+}
+
